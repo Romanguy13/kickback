e269f636f62e0dfc73cc60412a9fabe91a54f353
José Chavez <josmchavz@gmail.com>
2023-04-20 09:16:30 -0700

    Added a few more npm scripts for linting and specific testings

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { getDefaultConfig } = require('@expo/metro-config');
+// eslint-disable-next-line import/no-extraneous-dependencies,import/no-import-module-exports
+import { getDefaultConfig } from '@expo/metro-config';
 
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push('cjs');
