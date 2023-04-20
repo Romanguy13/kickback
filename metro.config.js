b297da8f5174f4100f48f205877e5bef83b1460a
Cody Lambert <codylambertdev@gmail.com>
2023-04-20 14:25:17 -0700

    metro config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
