fa5a69e8f8c750d6f7f943fc0570d4860720f5e5
Ricardo Esquivel <99927081+richybourne@users.noreply.github.com>
2023-04-28 23:06:31 -0700

    Fixed file for richy cause it was not running

--- src/screens/EventCard.tsx
+++ src/screens/EventCard.tsx
@@ -2,71 +2,59 @@ import React from 'react';
 import { TouchableWithoutFeedback, StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      padding: 2,
-      borderTopRightRadius: 60, 
-      borderBottomRightRadius: 60,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 2,
+    borderTopRightRadius: 60,
+    borderBottomRightRadius: 60,
+  },
+  date: {
+    fontWeight: '700',
+    fontSize: 20,
+    position: 'absolute',
+    width: 169,
+    height: 128,
+    left: 0,
+    top: 274,
+  },
+  item: {
+    paddingLeft: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingBottom: 5,
+  },
+  count: {
+    paddingLeft: 15,
+  },
+});
 
-    },
-    date: {
-      fontWeight: '700',
-      fontSize: 20,
-      position: 'absolute',
-      width: 169,
-      height: 128,
-      left: 0,
-      top: 274
-    },
-    item: {
-      paddingLeft: 10,
-      fontSize: 18,
-      fontWeight: 'bold',
-      paddingBottom: 5,
-    },
-    count: {
-      paddingLeft: 15,
-    },
-  });
+interface NavBarProps {
+  navigation: any;
+}
 
-  interface NavBarProps {
-    navigation: any; // Replace with the correct type for your navigation prop
-  }
+function EventCard({ event, navigation }: any) {
+  // accessibility labels
+  let timeLabel = 'time of the event';
+  let dateLabel = 'date';
+  let titleLabel = 'title of event';
+  let statusLabel = 'status';
+  let invitedLabel = '# of invites';
 
-
-  const EventCard = ({event, navigation} : any) => {
-
-    // accessibility labels 
-    let timeLabel = "time of the event";
-    let dateLabel = "date";
-    let titleLabel = "title of event";
-    let statusLabel = "status";
-    let invitedLabel = "# of invites";
-    
-    return (
-        <View>
-            <TouchableWithoutFeedback
-                onPress={() => navigation.navigate('Event Detail', {event: event})}>
-                <View style={styles.container}>
-                    <Text style={styles.container}
-                        accessibilityLabel={titleLabel}>
-                        {event.title}
-                    </Text>
-                    <Text
-                        accessibilityLabel={dateLabel}>
-                        {event.date}</Text>
-                    <Text
-                        accessibilityLabel={timeLabel}>
-                    {event.time}</Text>
-                    <Text
-                        accessibilityLabel={statusLabel}
-                    >{event.status}</Text>
-                    </View> 
-                </TouchableWithoutFeedback>
+  return (
+    <View>
+      <TouchableWithoutFeedback onPress={() => navigation.navigate('Event Detail', { event })}>
+        <View style={styles.container}>
+          <Text style={styles.container} accessibilityLabel={titleLabel}>
+            {event.title}
+          </Text>
+          <Text accessibilityLabel={dateLabel}>{event.date}</Text>
+          <Text accessibilityLabel={timeLabel}>{event.time}</Text>
+          <Text accessibilityLabel={statusLabel}>{event.status}</Text>
         </View>
-    )
-
-};
+      </TouchableWithoutFeedback>
+    </View>
+  );
+}
 
 export default EventCard;
