f1c85af065d45a44cf736fef69f96247c363625d
Jessica <jhuan221@ucsc.edu>
2023-05-02 21:44:19 -0700

    added event details file

---
+++ src/navigation/screens/EventDetail.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// import NavBar from '../NavBar';
+
+interface EventGroupsProps {
+  navigation: any;
+}
+
+export default function EventDetail({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF7000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    padding: 30,
+  },
+  text: {
+    color: '#FFFFFB',
+    fontSize: 70,
+    fontWeight: 'bold',
+    padding: 10,
+    width: '100%',
+  },
+});
