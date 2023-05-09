445199b1d39edfe92b9595b7eaaebcbb65faec20
Ricardo Esquivel <99927081+richybourne@users.noreply.github.com>
2023-05-08 18:46:32 -0700

    Updated changes for EventCreation

--- src/navigation/screens/EventCreation.tsx
+++ src/navigation/screens/EventCreation.tsx
@@ -41,7 +41,7 @@ export default function EventCreation({ navigation, route }: { navigation: any; 
   };
 
   // Date Picker Config
-  const handleEventDateChange = (event, selectedDate) => {
+  const handleEventDateChange = (selectedDate: string) => {
     const currentDate = selectedDate || eventDate;
     setShowDatePicker(false);
     setEventDate(moment(currentDate).format('MMM DD, YYYY'));
@@ -51,7 +51,7 @@ export default function EventCreation({ navigation, route }: { navigation: any; 
     setShowDatePicker(true);
   };
   // Time Picker Config
-  const handleEventTimeChange = (event, selectedTime) => {
+  const handleEventTimeChange = (selectedTime: string) => {
     const currentTime = selectedTime || eventTime;
     setShowTimePicker(false);
     setEventTime(moment(currentTime).format('h:mm A'));
@@ -266,6 +266,7 @@ export default function EventCreation({ navigation, route }: { navigation: any; 
                 mode="date"
                 display="default"
                 textColor="dark"
+                testID="date-picker"
                 onChange={handleEventDateChange}
               />
             )}
