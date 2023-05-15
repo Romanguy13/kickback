6b133d93a5b51ab46ce28870da1d4d307137c0b2
Jose Chavez <josmchavz@gmail.com>
2023-05-15 16:23:42 -0700

    Completed all code coverage.

--- src/tests/frontend/components/EventCard.test.tsx
+++ src/tests/frontend/components/EventCard.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react-native';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Alert, View } from 'react-native';
@@ -8,21 +8,21 @@ import EventCard from '../../../components/EventCard';
 const renderSimple = async () =>
   render(
     <EventCard
-      item={{
-        event: {
-          id: '12345',
-          hostId: '12346',
-          name: 'Test Event',
-          location: 'Test Location',
-          date: '10/10/2023',
-          time: '10:00',
-          gId: '12347',
-          createdAt: 'Test Date',
-          updatedAt: 'Test Time',
-        },
-        navigation: jest.fn(),
+      event={{
+        id: '12345',
+        hostId: '12346',
+        name: 'Test Event',
+        location: 'Test Location',
+        date: '10/10/2023',
+        time: '10:00',
+        gId: '12347',
+        createdAt: 'Test Date',
+        updatedAt: 'Test Time',
       }}
-    />    
+      navigation={{
+        navigate: jest.fn(),
+      }}
+    />
   );
 
 test('Renders Event Card', async () => {
@@ -32,4 +32,15 @@ test('Renders Event Card', async () => {
   expect(screen.getByText('Test Location')).toBeTruthy();
   expect(screen.getByText('10:00')).toBeTruthy();
   expect(screen.getByText('10/10/2023')).toBeTruthy();
+});
+
+test('Click Event Card', async () => {
+  await renderSimple();
+
+  expect(screen.getByText('Test Event')).toBeTruthy();
+  expect(screen.getByText('Test Location')).toBeTruthy();
+  expect(screen.getByText('10:00')).toBeTruthy();
+  expect(screen.getByText('10/10/2023')).toBeTruthy();
+
+  await fireEvent.press(screen.getByText('Test Event'));
 });
